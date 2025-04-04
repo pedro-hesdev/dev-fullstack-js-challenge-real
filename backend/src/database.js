bef36273bf0bead081d2d9f9f7dac96c039fe9e6
Pedro Eduardo <pedroeduardo171220@gmail.com>
2025-04-04 17:54:36 -0300

    Criando projeto node,instalando express , nodemon e endpoint para listar estudantes e buscar estudantes

---
+++ backend/src/database.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        nome:'Pedro Henrique Eduardo',
+        email:'pedro171220@gmail.com ',
+        cpf:'051.158.155-20',
+        ra:'5821852',   
+    },
+
+    {
+        nome:'Maria',
+        email:'maria1223@gmail.com',
+        cpf:'072.188.455-50',
+        ra:'587085562',   
+    },
+    
+    {
+        nome:'João',
+        email:'joao@gmail.com',
+        cpf:'381.359.111-55',
+        ra:'12684775',   
+    },
+    
+    {
+        nome:'Bruna',
+        email:'bruninha@gmail.com',
+        cpf:'121.289.555-35',
+        ra:'879218502',   
+    },
+
+];
