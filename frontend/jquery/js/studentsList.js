5f1a8101a8f8a68a1e31a48d15234dce02868f0d
Pedro Eduardo <pedroeduardo171220@gmail.com>
2025-04-05 12:56:54 -0300

    Intalação do cors , também começamos a trablhar com API da nossa pagina , para gerar os dados do estudantes e subir ela em nossa página.

---
+++ frontend/jquery/js/studentsList.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    fetshStudentsList();
+})
+
+function fetshStudentsList(){
+    fetch("http://localhost:3000/students/list")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        const table = $("#studentList tbody");
+        data.map(function(student){
+            table.append(`
+                
+                 <tr>
+                    <td>${student.ra}</td>
+                    <td>${student.nome}</td>
+                    <td>${student.cpf}</td>
+                    <td>
+                        <a href="#">Editar</a>
+                        <a href="#">Excluir</a>
+                    </td>
+                </tr>    
+                
+            `);
+        });
+
+    });
+
+}
